4d7a88b1361a0fe6b8bc52eb364bc4a6bca78f57
KEX-03 <killex003@outlook.com>
2026-07-08 10:44:03 +0530

    useEffect example for windowWidth change

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,19 @@
 import React, { useState, useEffect } from 'react'
-import './App.css'
 
 export default function App() {
-  const [resourceType, setResourceType] = useState('posts')
-  const [items, setItems] = useState([])
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth)
+
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth)
+  }
 
   useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
-      .then(response => response.json())
-      .then(json => setItems(json))
-  }, [resourceType])  
+    window.addEventListener('resize', handleResize)
+
+  }, [])  
 
   return (
-    <>
-      <div>
-        <button onClick = {() => setResourceType('posts')}>Posts</button>
-        <button onClick = {() => setResourceType('users')}>Users</button>
-        <button onClick = {() => setResourceType('comments')}>Comments</button>
-      </div>
-      <h1>{resourceType}</h1>
-      {items.map(item => {
-        return <pre>{JSON.stringify(item)}</pre>
-      })}
-    </>
+  
+      <div>{windowWidth}</div>
   )
 }
